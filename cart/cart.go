b64c48efce65c23ced28c268127368d8babece20
Ammar Al-Khawaldeh <me@ammar.dev>
2023-08-14 20:13:27 +0300

    Add quantity to cart product struct

--- cart/cart.go
+++ cart/cart.go
@@ -3,13 +3,13 @@ package cart
 import "github.com/ammardev/ecommerce-playground/products"
 
 type Cart struct {
-	ID       int64
+	ID        int64
 	SessionID string
-	Products []CartProduct
+	Products  []CartProduct
 }
 
 type CartProduct struct {
-	Product products.Product
+	Product         products.Product
+	Quantity        int
 	PriceOnAddition float64
 }
-
