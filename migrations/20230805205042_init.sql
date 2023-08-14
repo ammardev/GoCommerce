cfe3f36eb5759e52671296faf1a364f43b7028c3
Ammar Al-Khawaldeh <me@ammar.dev>
2023-08-14 20:13:13 +0300

    Add cart related tables

--- migrations/20230805205042_init.sql
+++ migrations/20230805205042_init.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-create table products (
+create table if not exists products (
     id int not null auto_increment primary key,
     title varchar(255) not null,
     description text not null,
@@ -8,7 +8,36 @@ create table products (
 );
 -- +goose StatementEnd
 
+-- +goose StatementBegin
+create table if not exists carts (
+    id int not null auto_increment primary key,
+    session_id varchar(255) not null
+);
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+create table if not exists cart_products (
+    product_id int not null,
+    cart_id int not null,
+
+    quantity int not null,
+    price int not null,
+
+    primary key (product_id, cart_id),
+    constraint fk_product_id foreign key (product_id) references products(id),
+    constraint fk_cart_id foreign key (cart_id) references carts(id)
+);
+-- +goose StatementEnd
+
 -- +goose Down
+-- +goose StatementBegin
+drop table cart_products;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+drop table carts;
+-- +goose StatementEnd
+
 -- +goose StatementBegin
 drop table products;
 -- +goose StatementEnd
