09b81119230b9fd2f84196c353dfef521f58d853
Ammar Al-Khawaldeh <me@ammar.dev>
2023-08-16 07:59:14 +0300

    Update handlers and initialize handlers methods

--- cart/handlers.go
+++ cart/handlers.go
@@ -3,7 +3,26 @@ package cart
 import "github.com/labstack/echo/v4"
 
 func RegisterRoutes(router *echo.Echo) {
-	router.GET("/carts/:sessionID", nil)
-	router.POST("/carts/:sessionID/products", nil)
-	router.DELETE("/carts/:sessionID/products/:id", nil)
+	router.GET("/cart", getCart)
+	router.POST("/cart", addCartItem)
+	router.PATCH("/cart/:cartItemId", changeCartItemQuantity)
+	router.DELETE("/cart/:cartItemId", deleteCartItem)
+}
+
+func getCart(c echo.Context) error {
+	// TODO: Return the cart. Get the session id from the headers
+	return echo.ErrNotImplemented
+}
+
+func addCartItem(c echo.Context) error {
+	// TODO: Create a new cart if the header not exist. And add the item
+	return echo.ErrNotImplemented
+}
+
+func changeCartItemQuantity(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func deleteCartItem(c echo.Context) error {
+	return echo.ErrNotImplemented
 }
