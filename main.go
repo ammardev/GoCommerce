b6a8162840700382d8632102845866c43a7dcf22
Ammar Al-Khawaldeh <me@ammar.dev>
2023-08-16 17:53:04 +0300

    Register cart routes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/ammardev/ecommerce-playground/cart"
 	"github.com/ammardev/ecommerce-playground/connections"
 	"github.com/ammardev/ecommerce-playground/products"
 	"github.com/labstack/echo/v4"
@@ -13,6 +14,7 @@ func main() {
 	router := echo.New()
 
 	products.RegisterRoutes(router)
+	cart.RegisterRoutes(router)
 
 	router.Logger.Fatal(router.Start(":3000"))
 }
