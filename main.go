4d89bf24a2cb3989333b0607d7f60d993c076b32
Ammar Al-Khawaldeh <me@ammar.dev>
2023-08-28 22:16:41 +0300

    Handle validation erros

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/ammardev/ecommerce-playground/app"
 	"github.com/ammardev/ecommerce-playground/cart"
 	"github.com/ammardev/ecommerce-playground/connections"
 	"github.com/ammardev/ecommerce-playground/products"
@@ -16,8 +19,29 @@ func main() {
 
 	router.Use(middleware.Recover())
 
+	app.NewValidator()
+
+	router.HTTPErrorHandler = customHTTPErrorHandler
+
 	products.RegisterRoutes(router)
 	cart.RegisterRoutes(router)
 
 	router.Logger.Fatal(router.Start(":3000"))
+}
+
+func customHTTPErrorHandler(err error, c echo.Context) {
+	c.Logger().Error(err)
+
+	switch err.(type) {
+	case *app.ValidationError:
+		c.JSON(http.StatusUnprocessableEntity, err)
+	case *echo.HTTPError:
+		c.JSON(http.StatusInternalServerError, echo.HTTPError{
+			Message: err.Error(),
+		})
+	default:
+		c.JSON(http.StatusInternalServerError, echo.HTTPError{
+			Message: "Internal Error",
+		})
+	}
 }
