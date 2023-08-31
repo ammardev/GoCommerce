5a72493f39a9d00cba517bb576a5fdfe7a63adae
Ammar Al-Khawaldeh <me@ammar.dev>
2023-08-31 23:49:58 +0300

    Remove unused old function

--- pkg/products/product.go
+++ pkg/products/product.go
@@ -1,19 +1,10 @@
 package products
 
-import (
-	"github.com/ammardev/gocommerce/internal/connections"
-)
-
 type Product struct {
 	ID          int64   `json:"id"`
 	Title       string  `json:"title"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
-}
-
-func (product *Product) Update() error {
-	_, err := connections.DB.NamedExec("update products set title=:title, description=:description, price=:price where id=:id", &product)
-	return err
 }
 
 type Products []Product
